ada86e85435cd273c76cf17704f18de41f038c98
Alumno <Alumno@PC181>
2024-04-12 11:38:21 -0300

    12/04

--- clsNave.cs
+++ clsNave.cs
@@ -14,7 +14,8 @@ namespace pryMolina
         public string nombre;
         int puntosDaños;
         public PictureBox imgNave;
-        
+        public PictureBox imgEne;
+
         public void crearJugador()
         {
             vida = 100;
@@ -22,13 +23,33 @@ namespace pryMolina
             puntosDaños = 1;
             imgNave = new PictureBox();
             imgNave.SizeMode = PictureBoxSizeMode.StretchImage;
-            imgNave.ImageLocation = "https://cdna.artstation.com/p/assets/images/images/017/509/756/large/rutger-slopsema-galaga.jpg?1556264895";
+            imgNave.ImageLocation = "https://gifdb.com/images/high/galaga-ship-gyaraga-art-48fvhsarxw0j2oku.webp";
         }
         public void crearEnemigo()
         {
+            //Enemigo 1
             vida = 1000;
             nombre = "Enemigo";
             puntosDaños = 1;
+            imgEne= new PictureBox();
+            imgEne.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgEne.ImageLocation = "https://gifdb.com/images/high/galaga-boss-game-sticker-te0hoq27fvtgnpv6.webp";
+
+            //Enemigo 2
+            vida = 1000;
+            nombre = "Enemigo2";
+            puntosDaños = 1;
+            imgEne = new PictureBox();
+            imgEne.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgEne.ImageLocation = "https://art.pixilart.com/5096bbb7736c227.gif";
+
+            //Enemigo 3
+            vida = 1000;
+            nombre = "Enemigo3";
+            puntosDaños = 1;
+            imgEne = new PictureBox();
+            imgEne.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgEne.ImageLocation = "https://art.pixilart.com/5ce2059e8521.gif";
         }
     }
 }
